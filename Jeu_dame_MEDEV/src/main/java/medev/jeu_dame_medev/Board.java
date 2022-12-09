1860a9a0da260224c79afe952eb69cb71d99a4c1
Corentin <coco@clementmacfarlane.fr>
2022-12-09 11:00:44 +0100

    Initialisation

---
+++ Jeu_dame_MEDEV/src/main/java/medev/jeu_dame_medev/Board.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package medev.jeu_dame_medev;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author coco
+ */
+public class Board {
+    
+    private ArrayList<Piece> board;
+    
+    
+    
+}
---
+++ Jeu_dame_MEDEV/src/main/java/medev/jeu_dame_medev/Dame.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package medev.jeu_dame_medev;
+
+/**
+ *
+ * @author coco
+ */
+public class Dame extends Piece {
+    
+}
